a40833a07b454ecbad15f04d30731b489ceebb60
dimzles <dimzlestv@gmail.com>
2023-05-06 23:39:52 +0100

    Add recursive solution

--- fibonacci.js
+++ fibonacci.js
@@ -11,5 +11,21 @@ function fibs(n) {
   for (let i = 2; i < n; i++) {
     array.push(array[array.length - 2] + array[array.length - 1]);
   }
+
   return array;
+}
+
+/**
+ *  Recursive solution to fibonacci sequence
+ *  @param {number} n - the amount of numbers from the sequence to return
+ *  @returns an array of elements with n amount of numbers from the sequence
+ */
+
+function fibsRec(n, array = [0, 1]) {
+  if (array.length >= n) return array.slice(0, n);
+
+  const product = array[array.length - 2] + array[array.length - 1];
+  array.push(product);
+
+  return fibsRec(n, array);
 }
